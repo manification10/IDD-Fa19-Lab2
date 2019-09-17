4c0c17f79ff217077203d1466d7d2bd97c5ede1a
Ananya Paul <manification10@users.noreply.github.com>
2019-09-16 22:02:23 -0400

    Updated code

--- own_timer.ino
+++ own_timer.ino
@@ -9,6 +9,22 @@
 
   http://www.arduino.cc/en/Tutorial/AnalogReadSerial
 */
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+int lostMelody[] = {
+    NOTE_G3, 0, NOTE_G3, NOTE_G3,  NOTE_A3, NOTE_C4,NOTE_C3, NOTE_C2
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+
 const int buttonPin = 2; 
 const int ledPin =  13;    
 int buttonState = 0; 
@@ -26,20 +42,53 @@ void setup() {
   delayStart = millis();   // start delay
   delayRunning = true; // not finished yet
 }
+void winTune() {
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
+void loserTune() {
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, lostMelody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
 
 // the loop routine runs over and over again forever:
 void loop() {
   if (gameState == 1 && delayRunning && ((millis() - delayStart) >= 5000)) {
     delayRunning = false; // // prevent this code being run more then once
-    Serial.println("Game Over");
+    Serial.println("Game Over, you lost. :(");
     gameState = 0;
+    loserTune();
   }
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     gameState = 1;
-    Serial.println("Start Game");
+    Serial.println("Start Game, get to 100 in 5 seconds");
     delayStart = millis();
     delayRunning = true;
     // turn LED on:
@@ -51,7 +100,7 @@ void loop() {
 
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  if (gameState == 1 && sensorValue < 101 && sensorValue > 99) 
+  if (gameState == 1 && sensorValue == 100) 
   {
     Serial.println(sensorValue);
     Serial.println("Yaay! You won in:");
@@ -59,6 +108,7 @@ void loop() {
     Serial.println(winTime);
     Serial.println("seconds");
     gameState = 0;
+    winTune();
   }
   else if (gameState == 1)
   {
